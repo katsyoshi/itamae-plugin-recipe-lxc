3c9da85818e809bf0a89eae7e97a99d17a8d8b80
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2016-04-25 20:15:04 +0900

    fix use dig

--- lib/itamae/plugin/recipe/lxc/system.rb
+++ lib/itamae/plugin/recipe/lxc/system.rb
@@ -1,42 +1,42 @@
-create_args = %w(lxc create template)
-template_name = node.dig(*create_args, 'name')
-options = node.dig(*create_args, 'options')&.map{|key, value| "--#{key.to_s} #{value}" }&.join(' ')
-name = node[:lxc][:name]
-
-node.dig('lxc', 'create')&.tap do |_create|
-  execute "lxc-create -t #{template_name} -n #{name} -- #{options}" do
-    command "lxc-create -t #{template_name} -n #{name} -- #{options}"
-    not_if "lxc-ls | grep #{name}"
+node.dig('lxc')&.tap do |lxc|
+  name = lxc.dig('name')
+  lxc.dig('create')&.tap do |create|
+    template_name = create.dig('template', 'os')
+    options = create.dig('template', 'options')&.map{|key, value| "--#{key.to_s} #{value}" }.join(' ')
+    execute "lxc-create -t #{template_name} -n #{name} -- #{options}" do
+      command "lxc-create -t #{template_name} -n #{name} -- #{options}"
+      not_if "lxc-ls | grep #{name}"
+    end
   end
-end
 
-master = node.dig('lxc', 'clone', 'master')
-copy = node.dig('lxc', 'clone', 'copy')
+  master = lxc.dig('clone', 'master') || name
+  copy = lxc.dig('clone', 'copy')
 
-node.dig('lxc', 'clone')&.tap do |_clone|
-  execute "lxc-clone #{master} #{copy}" do
-    command "lxc-clone #{master} #{copy}"
-    not_if "lxc-ls | grep #{copy}"
+  lxc.dig('clone')&.tap do |_clone|
+    execute "lxc-clone #{master} #{copy}" do
+      command "lxc-clone #{master} #{copy}"
+      not_if "lxc-ls | grep #{copy}"
+    end
   end
-end
 
-node.dig('lxc', 'start')&.tap do |start|
-  execute "lxc-start -n #{name}" do
-    command "lxc-start -n #{name}"
-    not_if "test -z #{start}"
+  lxc.dig('start')&.tap do |start|
+    execute "lxc-start -n #{name}" do
+      command "lxc-start -n #{name}"
+      not_if "test -z #{start}"
+    end
   end
-end
 
-node.dig('lxc', 'stop')&.tap do |stop|
-  execute "lxc-stop -n #{name}" do
-    command "lxc-stop -n #{name}"
-    not_if "test -z #{stop}"
+  lxc.dig('stop')&.tap do |stop|
+    execute "lxc-stop -n #{name}" do
+      command "lxc-stop -n #{name}"
+      not_if "test -z #{stop}"
+    end
   end
-end
 
-node.dig('lxc', 'destroy')&.tap do |destroy|
-  execute "lxc-destroy -n #{name}" do
-    command "lxc-destroy -n #{name}"
-    not_if "lxc-ls | grep #{name}" && "test -z #{destroy}"
+  lxc.dig('destroy')&.tap do |destroy|
+    execute "lxc-destroy -n #{name}" do
+      command "lxc-destroy -n #{name}"
+      not_if "lxc-ls | grep #{name}" && "test -z #{destroy}"
+    end
   end
 end
